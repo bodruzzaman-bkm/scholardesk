d955620d4523959a65403a051feb510e411caad8
B K M Bodruzzaman <bkmbodruzzaman@gmail.com>
2026-08-14 00:30:47 +0600

    Minnor Updates

--- database/migrations/2026_08_11_151357_create_collection_paper_table.php
+++ database/migrations/2026_08_11_151357_create_collection_paper_table.php
@@ -6,25 +6,16 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('collection_paper', function (Blueprint $table) {
             $table->id();
-            
-            // Foreign Keys: Linking collections and papers
             $table->foreignId('collection_id')->constrained()->onDelete('cascade');
             $table->foreignId('paper_id')->constrained()->onDelete('cascade');
-            
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('collection_paper');
